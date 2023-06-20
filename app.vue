474a4084e9f387e13d3f7b2ec87f1bd6bd3cd764
Hal Heisler <hal@sig9.dev>
2023-06-19 17:44:43 -0700

    Added nuxt-img example and a dir listing utility for swa

--- app.vue
+++ app.vue
@@ -1,5 +1,21 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <nuxt-img height="200" src="/images/feature/test.png" />
+    <br/>
+    <button :click="refresh">Refresh SWA Directories</button>
+    <br/>
+    error message:<br/><pre>{{ data?.errormsg }}</pre>
+    errors<br/><pre>{{ data?.errors }}</pre>
+    <br/>tree<br><pre>{{ data?.tree }}</pre>
+    
   </div>
 </template>
+
+<script setup lang="ts">
+
+const {data, refresh } = await useFetch('/api/ls', {method: 'GET'})
+
+
+
+</script>
+
---
+++ server/api/ls.get.ts
@@ -0,0 +1,52 @@
+import { fileURLToPath } from 'url'
+import * as path from 'path'
+import * as fs from 'fs'
+
+export default defineEventHandler((event): { errormsg: string, filename: string, dirname: string, tree: string, errors: any[] } => {
+  let errors = []
+  let tree = ''
+  let errormsg = ''
+  try {
+    const treeData = lstree('/home') // lstree(path.dirname(fileURLToPath(new URL(import.meta.url))))
+    errors = treeData.errors
+    tree = treeData.treeString
+  } catch (error) {
+    const errormsg = JSON.stringify(error)
+  }
+
+  const filename = fileURLToPath(new URL(import.meta.url))
+  const dirname = path.dirname(fileURLToPath(new URL(import.meta.url)))
+  return {
+    errormsg,
+    filename,
+    dirname,
+    tree,
+    errors,
+  }
+})
+
+function lstree(dir: string, indent = '', stringBuffer: string[] = [], errors: any[] = []) {
+  try {
+    const files = fs.readdirSync(dir)
+    files.forEach((file) => {
+      const filePath = path.join(dir, file)
+      let isDirectory = false
+
+      try {
+        isDirectory = fs.statSync(filePath).isDirectory()
+      } catch (err) {
+        errors.push({ path: filePath, message: JSON.stringify(err) })
+      }
+
+      stringBuffer.push(indent + file + '\n')
+
+      if (isDirectory) {
+        lstree(filePath, indent + ' ', stringBuffer, errors)
+      }
+    })
+  } catch (err) {
+    errors.push({ path: dir, message: JSON.stringify(err) })
+  }
+
+  return { treeString: stringBuffer.join(''), errors }
+}
